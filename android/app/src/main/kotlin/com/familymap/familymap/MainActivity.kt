6b49b9e91570240ad5f291945fd54ec4b3a60b4d
FamilyMap <familymap@dev.local>
2026-06-23 22:25:37 +0800

    feat: 后台保活 (AlarmManager 5分钟检测 + 被杀自动拉起)

--- android/app/src/main/kotlin/com/familymap/familymap/MainActivity.kt
+++ android/app/src/main/kotlin/com/familymap/familymap/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.familymap.familymap
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 启动时设定时保活
+        BootReceiver.scheduleNext(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // 每次回到前台都刷新定时器
+        BootReceiver.scheduleNext(this)
+    }
+}
